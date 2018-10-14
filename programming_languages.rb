00dc2752edfb890813c7866b12d26d38b49cb3c5
Sam B <s77broz@aol.com>
2018-10-14 14:41:54 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   
   languages.each do |style, lang|
     lang.each do |k, val|
-      out[k] = val
+      out[k] = out[k] || val
       out[k][:style] = style
     end
   end
